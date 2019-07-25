ccc003920fd2eb1a9a2dd32271876e86592cd55e
Jonas Beyer <jonas.beyer@student.hpi.de>
2019-07-25 14:47:23 +0200

    Add post-hoc transformation for checking conformance with SLA date

---
+++ log-transformer/src/main/java/de/hpi/bpt/transformation/posthoc/DateBeforeTransformation.java
@@ -0,0 +1,38 @@
+package de.hpi.bpt.transformation.posthoc;
+
+import de.hpi.bpt.datastructures.RowCaseLog;
+
+import java.util.Date;
+
+public class DateBeforeTransformation {
+
+    private String firstAttributeName;
+    private String secondAttributeName;
+
+    public DateBeforeTransformation(String firstAttributeName, String secondAttributeName) {
+        this.firstAttributeName = firstAttributeName;
+        this.secondAttributeName = secondAttributeName;
+    }
+
+    public void transform(RowCaseLog caseLog) {
+        var schema = caseLog.getSchema();
+        if (!Date.class.equals(schema.get(firstAttributeName).getType())
+                || !Date.class.equals(schema.get(secondAttributeName).getType())) {
+            throw new RuntimeException(String.format("One of [%s, %s] is not of type date", firstAttributeName, secondAttributeName));
+        }
+
+        var firstAttributeIndex = schema.get(firstAttributeName).getId();
+        var secondAttributeIndex = schema.get(secondAttributeName).getId();
+        schema.addColumnDefinition(String.format("%s_before_%s", firstAttributeName, secondAttributeName), Boolean.class);
+        caseLog.forEach((caseId, row) -> {
+            var firstDate = (Date) row.get(firstAttributeIndex);
+            var secondDate = (Date) row.get(secondAttributeIndex);
+
+            if (firstDate == null || secondDate == null) {
+                row.add(null);
+            } else {
+                row.add(firstDate.before(secondDate));
+            }
+        });
+    }
+}
---
+++ log-transformer/src/test/java/de/hpi/bpt/transformation/posthoc/DateBeforeTransformationTest.java
@@ -0,0 +1,36 @@
+package de.hpi.bpt.transformation.posthoc;
+
+import de.hpi.bpt.datastructures.RowCaseLog;
+import de.hpi.bpt.datastructures.Schema;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DateBeforeTransformationTest {
+
+    @Test
+    void transform() {
+        // Arrange
+        var schema = new Schema();
+        schema.addColumnDefinition("caseId", String.class);
+        schema.addColumnDefinition("date1", Date.class);
+        schema.addColumnDefinition("date2", Date.class);
+
+        var rowCaseLog = new RowCaseLog("Test", schema);
+        rowCaseLog.put("1", Stream.of("1", new Date(100), new Date(200)).collect(toList()));
+        rowCaseLog.put("2", Stream.of("2", new Date(200), new Date(200)).collect(toList()));
+        rowCaseLog.put("3", Stream.of("3", new Date(300), new Date(200)).collect(toList()));
+
+        // Act
+        new DateBeforeTransformation("date1", "date2").transform(rowCaseLog);
+
+        // Assert
+        assertThat(rowCaseLog.get("1")).containsExactly("1", new Date(100), new Date(200), true);
+        assertThat(rowCaseLog.get("2")).containsExactly("2", new Date(200), new Date(200), false);
+        assertThat(rowCaseLog.get("3")).containsExactly("3", new Date(300), new Date(200), false);
+    }
+}
